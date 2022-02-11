19f607547bf1bb648901f3c57629b7bc07fde597
lukkasz323 <39344794+lukkasz323@users.noreply.github.com>
2022-02-11 08:25:37 +0100

    added core elements to html / css / js + canvas

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
-// JS
+'use strict';
+
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d', {alpha: false}, {antialias: false});
+const width = 400;
+const height = 400;
+
+function renderBackground() {
+    context.fillStyle = 'green';
+    context.fillRect(0, 0, width, height);
+}
+
+renderBackground();
+
+// setInterval(() => {
+//     renderBackground();
+// }, 1000 / 60)
